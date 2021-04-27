a35191aa785ce1494058cda61493dbe5069fde4f
Olha Fastiuk <olhafastiuk@gmail.com>
2021-04-28 00:15:09 +0300

    Add header desktop style

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$('.header__lang-wrapp').click( function(){
+    $('.header__lang').toggleClass('change-lang');
+});
+
+$('.header__play').click( function(){
+    $('.header__play-btn').toggleClass('play');
+});
