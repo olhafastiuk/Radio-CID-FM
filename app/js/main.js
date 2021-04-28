f496b4dd91425e15cec10767e507dcb7bb7986dc
Olha Fastiuk <olhafastiuk@gmail.com>
2021-04-28 14:34:42 +0300

    Add section1 home page desktop style

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,7 @@ $('.header__lang-wrapp').click( function(){
 $('.header__play').click( function(){
     $('.header__play-btn').toggleClass('play');
 });
+
+$('.top__inner__play').click( function(){
+    $('.top__inner__play-btn').toggleClass('playBig');
+});
